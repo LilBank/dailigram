9f58f4eb6c17a47b2ffd0260b8471b71fc50be8c
bankkeez <sethitikul@gmail.com>
2018-10-17 19:44:40 +0700

    diary model (not done)

--- dailigram/settings.py
+++ dailigram/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asis/Bangkok'
+TIME_ZONE = 'Asia/Bangkok'
 
 USE_I18N = True
 
--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.urls import reverse
+import datetime
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=200, help_text='Enter a tag (e.g. Love, Happy, Sad)')
+    
+    def __str__(self):
+        return self.name
+
+class Page(models.Model):
+    diary = models.ForeignKey('Diary', on_delete=models.SET_NULL, null=True)  
+    story = models.CharField(max_length=1000, help_text='Write your story.')
+    tag = models.ManyToManyField(Tag, help_text='Select a tag')
+    date = datetime.date.today()
+    def __str__(self):
+        return str(self.date)
+    
+    def get_absolute_url(self):
+        return reverse('diary-detail', args=[str(self.id)])
+
+class Diary(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    
+    def get_absolute_url(self):
+        return reverse('user-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.first_name
