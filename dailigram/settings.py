d457552c53ff3f86143385f02afb865aaf91302b
bankkeez <sethitikul@gmail.com>
2018-10-17 17:12:17 +0700

    added urls.py

--- dailigram/settings.py
+++ dailigram/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'diary.apps.DiaryConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asis/Bangkok'
 
 USE_I18N = True
 
