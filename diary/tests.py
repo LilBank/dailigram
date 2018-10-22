f2f7e6c45c1d88b2ffbb8e874a7c6757a2a51910
Vichaphol Thamsuthikul <vichaphol.t@ku.th>
2018-10-22 23:01:34 +0700

    add simple test

--- diary/tests.py
+++ diary/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase , TransactionTestCase
+from django.urls import reverse 
+from .models import Page,Diary
 
-# Create your tests here.
+class IndexTestCase(TestCase):
+    def test_diary_exist(self):
+        response = self.client.get(reverse('diary:index'))
+        self.assertEqual(response.status_code, 200)
+
+class TestsThatDependsOnPrimaryKeySequences(TransactionTestCase):
+    reset_sequences = True
+
+    def test_page_pk(self):
+        page = Page.objects.create(story = "advanture")
+        diary = Diary.objects.create(first_name = "tintin")
+        self.assertEqual(page.pk + diary.pk, 2)
+        
+
+
+
+
+
