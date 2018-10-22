d58c7279b42d12264ff7337d1e6e86a9cd87b2b8
bankkeez <sethitikul@gmail.com>
2018-10-22 17:05:26 +0700

    added index.html

--- diary/urls.py
+++ diary/urls.py
@@ -3,5 +3,5 @@ from diary import views
 
 
 urlpatterns = [
-    
+    path('', views.index, name='index'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from diary.models import Tag, Page, Diary
 
-# Create your views here.
+def index(request):
+    num_pages = Page.objects.all().count()
+    num_tags = Tag.objects.count()   
+    num_diarys = Diary.objects.count()
+    
+    context = {
+        'num_diarys':  num_diarys,
+        'num_pages': num_pages,
+        'num_tags': num_tags,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
