eb7dc8d4d8bfa880ce82d26a98fc24e283c17aeb
Vichaphol Thamsuthikul <vichaphol.t@ku.th>
2018-10-23 14:53:21 +0700

    add javadoc

--- diary/tests.py
+++ diary/tests.py
@@ -4,13 +4,21 @@ from .models import Page,Diary
 
 class IndexTestCase(TestCase):
     def test_diary_exist(self):
+        """
+        Test diary existance by the response status code.
+        """
         response = self.client.get(reverse('diary:index'))
         self.assertEqual(response.status_code, 200)
 
-class TestsThatDependsOnPrimaryKeySequences(TransactionTestCase):
+    
+
+class PrimaryKeyTestCase(TransactionTestCase):
     reset_sequences = True
 
     def test_page_pk(self):
+        """
+        The primary key should be one for each object's creation. 
+        """
         page = Page.objects.create(story = "advanture")
         diary = Diary.objects.create(first_name = "tintin")
         self.assertEqual(page.pk + diary.pk, 2)
