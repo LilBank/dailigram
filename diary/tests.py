10ca2c75eba179991f5c0fa41a7353abf3e3e911
Vichaphol Thamsuthikul <vichaphol.t@ku.th>
2018-10-24 01:52:25 +0700

    more efficient

--- diary/tests.py
+++ diary/tests.py
@@ -1,10 +1,6 @@
-from django.test import TestCase , TransactionTestCase
-from django.urls import reverse 
+from django.test import TestCase 
+from django.urls import reverse
 from diary.models import Page,Diary
-from django.db import connection
-from django.db.utils import OperationalError
-from django.core.management.base import BaseCommand
-import time
 
 class TestingModels(TestCase):
 
@@ -12,8 +8,8 @@ class TestingModels(TestCase):
         """
         Test the string representation
         """
-        page = Page(diary = 'tony')
-        self.assertEqual(str(page), page.diary)
+        diary = Diary(first_name = 'tony')
+        self.assertEqual(str(diary), diary.first_name)
 
     def test_page_pk(self):
         """
@@ -26,16 +22,19 @@ class TestingModels(TestCase):
         """
         Test the number of object in the list
         """
-        num_pages = Page.objects.all().count()
-        self.assertEqual(num_pages,0)
+        num_diary = Diary.objects.all().count()
+        self.assertEqual(num_diary,0)
 
 class TestingViews(TestCase):
-    def test_setup(self):
+
+    def test_connection(self):
         """
         Test diary existance by the response status code.
         """
+        diary = Diary(first_name = 'tony')
         response = self.client.get(reverse('diary:index'))
         self.assertEqual(response.status_code, 200)
+        self.assertIn(diary.first_name, response.content)
 
 class TestingForms(TestCase):
     def test_setup1(self):
@@ -49,23 +48,6 @@ class TestingAPI(TestCase):
         Test 
         """
 
-class Command(BaseCommand):
-    """Django command that waits for database to be available"""
-
-    def handle(self, *args, **options):
-        """Handle the command"""
-        self.stdout.write('Waiting for database...')
-        db_conn = None
-        while not db_conn:
-            try:
-                connection.ensure_connection()
-                db_conn = True
-            except OperationalError:
-                self.stdout.write('Database unavailable, waiting 1 second...')
-                time.sleep(1)
-
-        self.stdout.write(self.style.SUCCESS('Database available!'))
-    
 
     
 
