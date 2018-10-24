b02303b04bfab0a3d86c18880519e3e3df12f24c
Vichaphol Thamsuthikul <vichaphol.t@ku.th>
2018-10-25 02:53:09 +0700

    change from ManyToMany to ForeignKey field

--- diary/views.py
+++ diary/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from diary.models import Tag, Page, Diary
 
 def index(request):
-    num_pages = Page.objects.all().count()
-    num_tags = Tag.objects.count()   
-    num_diarys = Diary.objects.count()
+    num_pages = Page.objects.all()
+    num_tags = Tag.objects.all()
+    num_diarys = Diary.objects.all()
     
     context = {
         'num_diarys':  num_diarys,
