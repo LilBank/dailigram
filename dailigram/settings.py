9df556b166ee4b3efab9e77a85a2a817d0ba8118
bankkeez <sethitikul@gmail.com>
2018-10-26 12:42:27 +0700

    test deploy

--- dailigram/settings.py
+++ dailigram/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dailigram.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
