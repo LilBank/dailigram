f2c74963c56ba20bee10b029d7596e1935247fe8
Vichaphol Thamsuthikul <vichaphol.t@ku.th>
2018-10-26 19:42:36 +0700

    change to generic view

--- diary/urls.py
+++ diary/urls.py
@@ -3,5 +3,5 @@ from diary import views
 
 app_name =  'diary'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
 from diary.models import Tag, Page, Diary
+from django.views import generic
 
-def index(request):
-    num_pages = Page.objects.all()
-    num_tags = Tag.objects.all()
-    num_diarys = Diary.objects.all()
+# def index(request):
+#     num_pages = Page.objects.all()
+#     num_tags = Tag.objects.all()
+#     num_diarys = Diary.objects.all()
     
-    context = {
-        'num_diarys': num_diarys,
-        'num_pages': num_pages,
-        'num_tags': num_tags,
-    }
+#     context = {
+#         'num_diarys': num_diarys,
+#         'num_pages': num_pages,
+#         'num_tags': num_tags,
+#     }
 
-    # Render the HTML template index.html with the data in the context variable
-    return render(request, 'index.html', context=context)
+#     # Render the HTML template index.html with the data in the context variable
+#     return render(request, 'index.html', context=context)
+
+class IndexView(generic.ListView):
+    template_name = 'diary/index.html'
+    context_object_name = 'all_diarys'
+
+    def get_queryset(self):
+        """
+        Return all of the objects in the list
+        """
+        return Diary.objects.all()
