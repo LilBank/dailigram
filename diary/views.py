c017143bc32f2f3a07aa3c705674ba5165e98967
bankkeez <sethitikul@gmail.com>
2018-10-26 12:55:32 +0700

    test deploy

--- diary/views.py
+++ diary/views.py
@@ -2,15 +2,14 @@ from django.shortcuts import render
 from diary.models import Tag, Page, Diary
 
 def index(request):
-    # num_pages = Page.objects.all()
-    # num_tags = Tag.objects.all()
-    # num_diarys = Diary.objects.all()
-    text = 'helloooo'
+    num_pages = Page.objects.all()
+    num_tags = Tag.objects.all()
+    num_diarys = Diary.objects.all()
+    
     context = {
-        # 'num_diarys': num_diarys,
-        # 'num_pages': num_pages,
-        # 'num_tags': num_tags,
-        text : text,
+        'num_diarys': num_diarys,
+        'num_pages': num_pages,
+        'num_tags': num_tags,
     }
 
     # Render the HTML template index.html with the data in the context variable
