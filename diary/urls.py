82fb2789e8d353a924d8da402ba6b56723410fe6
kitkit <kittin.vat@hotmail.com>
2018-10-27 17:57:23 +0700

    modified nav-bar

--- diary/urls.py
+++ diary/urls.py
@@ -4,5 +4,5 @@ from diary import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('create', views.create, name='create'),   
+    path('create', views.create, name='create'), 
 ]
