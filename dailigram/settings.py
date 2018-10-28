42cb6a8ec88bffb46da196c38b60dbddbf3a90f7
bankkeez <sethitikul@gmail.com>
2018-10-29 01:13:17 +0700

    edit .env

--- dailigram/settings.py
+++ dailigram/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'dailigram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),                      
+        'NAME': 'dailigramDB',                      
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
