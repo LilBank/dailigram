ab33066e0efc2606c3ec4fc1b096d50082513669
Vichaphol Thamsuthikul <vichaphol.t@ku.th>
2018-10-28 11:11:10 +0700

    fixed bug

--- dailigram/settings.py
+++ dailigram/settings.py
@@ -12,6 +12,7 @@ from decouple import config
 import django_heroku
 
 SECRET_KEY = config('SECRET_KEY')
+
 SOCIAL_AUTH_GITHUB_KEY = config('SOCIAL_AUTH_GITHUB_KEY')
 SOCIAL_AUTH_GITHUB_SECRET = config('SOCIAL_AUTH_GITHUB_SECRET')
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
@@ -37,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'social_django',
     'diary.apps.DiaryConfig',
-    'social_django', # <- Here
 ]
 
 MIDDLEWARE = [
@@ -66,13 +66,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-<<<<<<< HEAD
-                'social_django.context_processors.backends',  # <- Here
-                'social_django.context_processors.login_redirect', # <- Here
-=======
                 'social_django.context_processors.backends',
                 'social_django.context_processors.login_redirect',
->>>>>>> c1831a5ec4b412d9ee39178020665016f4ab96df
             ],
         },
     },
@@ -112,15 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-AUTHENTICATION_BACKENDS = (
- 'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
- 'social_core.backends.google.GoogleOpenId',  # for Google authentication
- 'social_core.backends.google.GoogleOAuth2',  # for Google authentication
- 'social_core.backends.github.GithubOAuth2',  # for Github authentication
- 'social_core.backends.facebook.FacebookOAuth2',  # for Facebook authentication
- 
- 'django.contrib.auth.backends.ModelBackend',
-)
 
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.open_id.OpenIdAuth',
