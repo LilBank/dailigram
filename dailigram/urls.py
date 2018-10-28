7ef3c03b57fea1676f7962289be1676c7025e646
bankkeez <sethitikul@gmail.com>
2018-10-29 00:43:19 +0700

    stuck on login required

--- dailigram/urls.py
+++ dailigram/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls import include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 # from diary import views
 
 urlpatterns = [
