0109a2641a5e18e2554edfbc27edcd79c997292c
Vichaphol Thamsuthikul <vichaphol.t@ku.th>
2018-10-29 23:06:57 +0700

    add view test

--- diary/tests.py
+++ diary/tests.py
@@ -1,30 +1,31 @@
-from django.test import TestCase 
+from django.test import TestCase
 from django.urls import reverse
-from diary.models import Page,Diary
+from diary.models import Page, Diary
 from django import forms
 from diary.forms import UserForm
+
 
 class TestingModels(TestCase):
 
     def test_diary_pk(self):
         """
-        Test that the primary key should be one for each object's creation. 
+        Test that the primary key should be one for each object's creation.
         """
-        diary = Diary.objects.create(first_name = 'tintin')
-        self.assertEqual(diary.pk ,1)
+        diary = Diary.objects.create(first_name='tintin')
+        self.assertEqual(diary.pk, 1)
 
     def test_string_representation(self):
         """
         Test that the string is correctly represented.
         """
-        diary = Diary.objects.create(first_name = 'tintin')
+        diary = Diary.objects.create(first_name='tintin')
         self.assertEqual(str(diary), diary.first_name)
 
     def test_diary_max_length(self):
         """
         Test that the max length of the field is equal or not.
         """
-        diary = Diary(first_name = 'tony')
+        diary = Diary(first_name='tony')
         max_length = diary._meta.get_field('first_name').max_length
         self.assertEquals(max_length, 100)
 
@@ -33,15 +34,20 @@ class TestingModels(TestCase):
         Test that the total number of object is 0 when nothing is created.
         """
         num_diary = Diary.objects.all().count()
-        self.assertEqual(num_diary,0)
+        self.assertEqual(num_diary, 0)
+
 
 class TestingViews(TestCase):
 
-    def test_connection(self):
+    def test_accessible_by_name(self):
         """
         Test diary's existance by the response status code.
         """
         response = self.client.get(reverse('diary:index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_accessible_by_location(self):
+        response = self.client.get('/diary/')
         self.assertEqual(response.status_code, 200)
 
     def test_no_diary_by_view(self):
