b5d708d35ef58f7ac91343894e7907676782f3fe
Vichaphol Thamsuthikul <vichaphol.t@ku.th>
2018-11-01 02:05:27 +0700

    add href to register button

--- diary/urls.py
+++ diary/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # ex: /diary/
     path('',  login_required(TemplateView.as_view(template_name="diary/index.html")), name='index'),
     # ex: /diary/register/
-    # path('register/', views.UserFormView.as_view(), name = 'register'),
+    path('register/', views.UserFormView.as_view(), name = 'register'),
     # ex: /diary/create/
     path('create/', views.CreateView.as_view(), name='create'),
 
--- diary/views.py
+++ diary/views.py
@@ -67,34 +67,34 @@ class CreateView(generic.CreateView):
     template_name = 'diary/create.html'
 
 
-# class UserFormView(View):
-#     form_class = UserForm
-#     template_name = 'diary/registration_form.html'
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'registration/registration_form.html'
 
-#     # display black form
-#     def get(self, request):
-#         form = self.form_class(None)
-#         return render(request, self.template_name, {'form': form})
+    # display black form
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
 
-#     # process form data
-#     def post(self, request):
-#         form = self.form_class(request.POST)
+    # process form data
+    def post(self, request):
+        form = self.form_class(request.POST)
 
-#         if form.is_valid():
-#             user = form.save(commit=False)
+        if form.is_valid():
+            user = form.save(commit=False)
 
-#             # cleaned data
-#             username = form.cleaned_data['username']
-#             password = form.cleaned_data['password']
-#             user.set_password(password)
-#             user.save()
+            # cleaned data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
 
-#             # return User objects if credential are correct
-#             user = authenticate(username=username, password=password)
+            # return User objects if credential are correct
+            user = authenticate(username=username, password=password)
 
-#             if user is not None:
-#                 if user.is_active:
-#                     login(request, user)
-#                     return redirect('diary:index')
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('diary:index')
 
-#         return render(request, self.template_name, {'form': form})
+        return render(request, self.template_name, {'form': form})
