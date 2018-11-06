ceaf804441c2415b2f3bf1b99215dfb6f9297cb6
bankkeez <sethitikul@gmail.com>
2018-11-06 23:19:33 +0700

    added docs for functions

--- utility/imgur.py
+++ utility/imgur.py
@@ -13,23 +13,50 @@ class ImgurUtil:
     imageHash = ''
 
     def get_all_homepage_image(*args):
+        """
+        Get all current images from Imgur homepage.
+        """
+         
         items = ImgurUtil.client.gallery()
         return items
 
     def get_image_info(self):
+        """
+        Get image info as json.
+        """
+
         url = 'https://api.imgur.com/3/album/' + ImgurUtil.albumHash + '/image/'+ ImgurUtil.imageHash
         headers = {'Authorization': 'Bearer ' + ImgurUtil.token }
         response = requests.request("GET", url, headers=headers)
         return response.json()
 
     def get_image_link(self):
+        """
+        Get the link of the image.
+        """
+
         return ImgurUtil.get_image_info('')['data']['link']
 
     def get_image_description(self):
+        """
+        Get the description of the image.
+        """
+
         return ImgurUtil.get_image_info('')['data']['description']
 
+    def create_album(self):
+
+
     def set_albumHash(self, hash):
+        """
+        Set album hash.
+        """
         ImgurUtil.albumHash = hash
 
     def set_imageHash(self, hash):
+        """
+        Set image hash.
+        """
+        
         ImgurUtil.imageHash = hash
+
