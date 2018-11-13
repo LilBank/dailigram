445f9be9a5a8f66ee1bb92c6ba5c5230fde25483
Vichaphol Thamsuthikul <vichaphol.t@ku.th>
2018-11-13 12:03:02 +0700

    fixed the picture's bugs

--- dailigram/urls.py
+++ dailigram/urls.py
@@ -30,4 +30,8 @@ urlpatterns = [
     path('auth/', include('social_django.urls', namespace='social')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', auth_views.LogoutView.as_view(),{'next_page' : settings.LOGOUT_REDIRECT_URL }, name='logout'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if settings.DEBUG: 
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
