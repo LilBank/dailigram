4fea731d743d7527e67db0acd973a24c68124817
bankkeez <sethitikul@gmail.com>
2018-11-17 14:48:07 +0700

    added files for coverage to init

--- dailigram/urls.py
+++ dailigram/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from django.views.generic import RedirectView
-from django.conf import settings
+# from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
@@ -31,6 +31,6 @@ urlpatterns = [
     # path('logout/', auth_views.LogoutView.as_view(),{'next_page' : settings.LOGOUT_REDIRECT_URL }, name='logout'),
 ]
 
-if settings.DEBUG: 
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+# if settings.DEBUG: 
+    # urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
     # urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
