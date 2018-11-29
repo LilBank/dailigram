80d2e341cea5c1781515b052bab9a807680859cf
Kittin Vatabutr <kittin.vat@hotmail.com>
2018-11-29 14:52:54 +0700

    new design for create page

--- diary/models.py
+++ diary/models.py
@@ -26,7 +26,7 @@ class Page(models.Model):
     diary = models.ForeignKey(Diary, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=20, blank=True)
     tag = models.ForeignKey(Tag, on_delete=models.SET_NULL, null=True)
-    story = models.TextField(help_text='Write your story.')
+    story = models.TextField()
     date = models.CharField(max_length=50, blank=True)
     picture = models.FileField()
 
